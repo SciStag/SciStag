00a549db41dc95e697cf257846979dd095a3f924
Michael Ikemann <[omitted]>
2022-10-27 00:24:30 +0200

    .

--- scistag/common/__init__.py
+++ scistag/common/__init__.py
@@ -1,5 +1,6 @@
 from .essential_data import ESSENTIAL_DATA_ARCHIVE_NAME, ESSENTIAL_DATA_URL, \
     get_edp, ESSENTIAL_DATA_SIZE
+from .cache import Cache
 from .component import Component
 from .config_stag import ConfigStag
 from .data_cache import DataCache
@@ -8,6 +9,7 @@ from .mt import StagLock
 
 __all__ = ["ESSENTIAL_DATA_ARCHIVE_NAME", "ESSENTIAL_DATA_URL", "get_edp",
            "ESSENTIAL_DATA_SIZE",
+           "Cache",
            "Component", "ConfigStag",
            "DataCache", "get_test_data", "get_test_image", "TestDataNames",
            "StagLock"]
