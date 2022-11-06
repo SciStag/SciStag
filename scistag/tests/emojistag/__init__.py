a378e96ae641b9db9375f9e5d6a85dc928665475
Michael Ikemann <[omitted]>
2022-11-06 09:07:11 +0100

    .

--- scistag/tests/emojistag/__init__.py
+++ scistag/tests/emojistag/__init__.py
@@ -3,11 +3,12 @@ Definition of the tests for the EmojiStag module
 """
 from scistag.tests.visual_test_log_scistag import VisualTestLogSciStag
 
-vl = VisualTestLogSciStag(test_filename=__file__)
+test_log = VisualTestLogSciStag(test_filename=__file__)
+vl = test_log.default_builder
 
 
 def teardown_module(_):
     """
     Finalize the test
     """
-    vl.finalize()
+    test_log.finalize()
