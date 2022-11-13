800beb6c90c63dbcace503c612b457e0cdc55b68
Michael Ikemann <[omitted]>
2022-11-13 14:22:00 +0100

    .

---
+++ scistag/tests/vislog/__init__.py
@@ -0,0 +1,16 @@
+"""
+Implements the tests for the vislog module
+"""
+
+from scistag.tests.visual_test_log_scistag import VisualTestLogSciStag
+
+test_log = VisualTestLogSciStag(test_filename=__file__,
+                                formats_out={"html", "md", "txt"})
+vl = test_log.default_builder
+
+
+def teardown_module(_):
+    """
+    Finalize the test
+    """
+    test_log.finalize()
