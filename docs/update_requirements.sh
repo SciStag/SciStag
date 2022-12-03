67fe728656e2517433505f2ed8dba8b7721cef64
Michael Ikemann <[omitted]>
2022-12-03 12:50:30 +0100

    * Added a new extra group "common" for a well weighted standard installation * Default testing environment is now "common" * Added group "full" for optional, large components such as Kivy, Qt or Jupyter

--- docs/update_requirements.sh
+++ docs/update_requirements.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-poetry run poetry export --with docu -E "full" -f requirements.txt -o ./docs/requirements.txt --without-hashes
+poetry run poetry export --with docu -E "common" -f requirements.txt -o ./docs/requirements.txt --without-hashes
