2dc70bb71688ab6274e33a0b7ce912eb3f3b137f
Michael Ikemann <[omitted]>
2022-12-28 21:32:02 +0100

    .

--- scistag/vislog/widgets/__init__.py
+++ scistag/vislog/widgets/__init__.py
@@ -2,6 +2,10 @@
 Defines the module for Widget extensions for VisualLog
 """
 
+from .event import LEvent
 from .log_widget import LWidget
-from .log_button import LButton
-from .log_event import LEvent
+from .timer import LTimer
+from .button import LButton
+from .slider import LSlider
+
+__all__ = ["LWidget", "LButton", "LSlider", "LTimer", "LEvent"]
